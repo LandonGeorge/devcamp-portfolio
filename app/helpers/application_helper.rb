4ba661e3657a6745464e84a42427d40f2279f220
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 15:57:00 +0000

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
      content_tag(:p, greeting, class: "source-greeting") 
     end 
   end
+  
+  def copyright_generator
+  GeorgeViewTool::Renderer.copyright 'Landon George', 'All rights reserved'
+  end
 end
